c494169132898f579448af13624fdb34671f28e1
remu <986563206@qq.com>
2021-09-16 10:37:35 +0800

    init

---
+++ backend/src/main/java/top/tocome/webservice/BackendApplication.java
@@ -0,0 +1,13 @@
+package top.tocome.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
