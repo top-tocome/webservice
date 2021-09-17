633cd1374b922ebde68a0edfeb8acbd0fe8ba823
remu <986563206@qq.com>
2021-09-17 15:55:20 +0800

    Add comments
    添加注释

--- backend/src/main/java/top/tocome/webservice/data/ResponseData.java
+++ backend/src/main/java/top/tocome/webservice/data/ResponseData.java
@@ -2,10 +2,22 @@ package top.tocome.webservice.data;
 
 import com.alibaba.fastjson.JSONObject;
 
+/**
+ * 返回给前端数据结构
+ * <p>
+ * 固定参数：<br>
+ * code: 0为请求成功，其他为请求失败<br>
+ * message: 错误消息
+ */
 public class ResponseData {
-
+    /**
+     * json对象
+     */
     protected JSONObject object = new JSONObject();
 
+    /**
+     * 默认为成功状态
+     */
     public ResponseData() {
         object.put("code", 0);
         object.put("message", "ok");
@@ -16,11 +28,21 @@ public class ResponseData {
         object.put("message", message);
     }
 
+    /**
+     * 新增要传输的数据
+     *
+     * @param key   关键词
+     * @param value 可序列化对象
+     * @return 链式调用
+     */
     public ResponseData put(String key, Object value) {
         object.put(key, value);
         return this;
     }
 
+    /**
+     * 序列化为json字符串
+     */
     public String toJSONString() {
         return object.toJSONString();
     }
--- backend/src/main/java/top/tocome/webservice/filemanager/FileAttribute.java
+++ backend/src/main/java/top/tocome/webservice/filemanager/FileAttribute.java
@@ -5,7 +5,13 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
+/**
+ * 要传输的文件属性
+ */
 public class FileAttribute {
+    /**
+     * {@link #time}的格式化方式
+     */
     public static final SimpleDateFormat dateFormat = new SimpleDateFormat("hh:mm:ss yyyy-MM-dd");
 
     public FileAttribute(String path) {
@@ -20,12 +26,32 @@ public class FileAttribute {
         time = dateFormat.format(new Date(file.lastModified()));
     }
 
+    /**
+     * 该文件是否为目录
+     */
     public boolean isDirectory;
+    /**
+     * 文件名称
+     */
     public String name;
+    /**
+     * 文件路径
+     */
     public String path;
+    /**
+     * 文件大小
+     */
     public long size;
+    /**
+     * 文件上次修改时间
+     */
     public String time;
 
+    /**
+     * 获取一个目录下的所有文件的属性
+     *
+     * @param path 文件夹路径
+     */
     public static ArrayList<FileAttribute> getAll(String path) {
         ArrayList<FileAttribute> fileAttributes = new ArrayList<>();
         File[] files = new File(path).listFiles();
