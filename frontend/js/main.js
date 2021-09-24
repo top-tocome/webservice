de75d0a34c0d77ad535c971fcb49931de8c84115
remu <986563206@qq.com>
2021-09-24 15:18:31 +0800

    use template

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,10 +1,13 @@
 //init
+
+
 $(document).ready(function () {
-        $("div.navbar").append(
-            `<a href="#">链接</a>
-    <a href="#">链接</a>
-    <a href="#">链接</a>
-    <a href="#" class="right">链接</a>`
-        )
+        //导航栏
+        $("div.navbar").append(`
+    <a href="./index.html">主页</a>
+    <a href="./files.html">文档管理</a>
+    <a href="./tools.html">脚本类工具</a>
+    <a href="./about.html">关于</a>
+    <a href="./login.html" class="right">登录</a>`)
     }
 )
