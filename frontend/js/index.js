01ae14cecc80780b961cb0629580884ce0a83084
remu <986563206@qq.com>
2021-09-25 19:49:51 +0800

    add article card

---
+++ frontend/js/index.js
@@ -0,0 +1,17 @@
+// https://picsum.photos/
+
+//文章卡片
+function card(title, desc, url) {
+    if (url == null) url = "https://picsum.photos/500/200?random=" + Math.random()
+    return `<a class="card" href="#">
+                <div class="front" style="background-image: url(${url});">
+                    <p>${title}</p>
+                </div>
+                <div class="back">
+                    <div>
+                        <p>${desc}</p>
+                        <button class="button">查看详情</button>
+                    </div>
+                </div>
+            </a>`
+}
